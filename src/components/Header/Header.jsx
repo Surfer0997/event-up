2a2529150d260300e33a0678efb0e86f1ad71783
Surfer0997 <artanagibaet228@gmail.com>
2023-02-17 22:01:51 +0200

    Home page event card dividers added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
             </ul>
           </nav>
         </div>
-        <p className="title"> Sed ut perspiciatis unde omnis</p>
+        <p className="title">Joyful events in your city</p>
       </div>
     </header>
   );
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = ({data}) => {
       {data.map((event) => (
         <Link className="card" key={event.id} href={`/events/${event.id}`} passHref>
       <div className="image">
-            <Image width={600} height={400} alt={event.title} src={event.image} />
+            <Image fill alt={event.title} src={event.image} loading="lazy" style={{objectFit:"cover"}}/>
           </div>
           <div className="content">
           <h2>{event.title}</h2>
